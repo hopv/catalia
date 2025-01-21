30e1e312d447f4b08c76e9663bd686def3954a5f
Hiroyuki Katsura <moratorium08@gmail.com>
2025-01-21 21:47:36 +0900

    wip: inline_tuple (abondoned)

--- src/preproc/inline_tuple.rs
+++ src/preproc/inline_tuple.rs
@@ -4,18 +4,24 @@
 //! If the tuple appears in a dtype, then we reconstruct the tuple
 
 use crate::common::*;
+use crate::info;
 use crate::instance::*;
 use crate::preproc::RedStrat;
 
 /// Calls `PredInstance::simplify_all`.
 pub struct InlineADT;
 
+struct Trans<'a, 'b> {
+    instance: &'a mut PreInstance<'b>,
+    typ2tuple: HashMap<DTyp, Vec<Typ>>,
+}
+
 /// Check if the given type is a tuple.
 ///
 /// Currently, very conservative. A tuple is composed of multiple items of base types.
 fn tuple_opt(typ: &DTyp) -> Option<Vec<Typ>> {
     let mut itr = typ.news.iter();
-    let types = if let Some((name, t)) = itr.next() {
+    let types = if let Some((_, t)) = itr.next() {
         if itr.next().is_some() {
             return None;
         }
@@ -33,15 +39,89 @@ fn tuple_opt(typ: &DTyp) -> Option<Vec<Typ>> {
     Some(vec)
 }
 
-fn expand_tuple(instance: &mut PreInstance, typ2tuple: &HashMap<Typ, Vec<Typ>>) {
-    // 0. redefine each predicate
-    // 1. iter all the clauses
-    // 2. if a variable is a tuple, then introduce new variables
-    // 3. replace the tuple access with the new variables
-    // 3.1 constructor is removed and inlined
-    // 3.2 tester is always true
-    // 3.3 accessor is replaced with the corresponding variable
-    // 3.4 if a variable appears as an argument of type tuple for a function, then we reconstruct it (e.g., a constructor for another dtype).
+impl<'a, 'b> Trans<'a, 'b> {
+    fn new(instance: &'a mut PreInstance<'b>) -> Self {
+        let mut typ2tup = HashMap::new(); // O(n) is OK
+        let mut n_types = 0; // count number of types for generating unique names later
+        for (name, typ) in dtyp::get_all().iter() {
+            n_types += 1;
+            match tuple_opt(typ) {
+                Some(types) => {
+                    println!("name: {}", name);
+                    println!("typ: {}", typ);
+                    println!("types: {:?}", types);
+                    typ2tup.insert(typ.clone(), types);
+                }
+                None => {}
+            }
+        }
+        Self {
+            instance,
+            typ2tuple: typ2tup,
+        }
+    }
+    fn get_tuple(&self, typ: &Typ) -> Option<&Vec<Typ>> {
+        match typ.get() {
+            typ::RTyp::DTyp { dtyp, prms } => match self.typ2tuple.get(dtyp) {
+                Some(ts) => Some(ts),
+                None => None,
+            },
+            _ => None,
+        }
+    }
+    fn work_on(&self, c: &Clause) -> Clause {
+        // 2. if a variable is a tuple, then introduce new variables
+        // 3. replace the tuple access with the new variables
+        // 3.1 constructor is removed and inlined
+        // 3.2 tester is always true
+        // 3.3 accessor is replaced with the corresponding variable
+        // 3.4 if a variable appears as an argument of type tuple for a function, then we reconstruct it (e.g., a constructor for another dtype).
+        let mut new_vars = VarMap::new();
+        for v in c.vars() {
+            match self.get_tuple(&v.typ) {
+                Some(ts) => {
+                    for (idx, t) in ts.iter().enumerate() {
+                        let info = info::VarInfo::new(
+                            format!("{}-{}", v.name, idx),
+                            t.clone(),
+                            new_vars.next_index(),
+                        );
+                        new_vars.push(info);
+                    }
+                }
+                None => {
+                    new_vars.push(v.clone());
+                }
+            }
+        }
+        unimplemented!()
+    }
+    fn expand_tuple(&mut self) {
+        // 0. redefine each predicate
+        let mut new_preds = VarMap::new();
+        for p in self.instance.preds() {
+            let mut new_sigs = VarMap::new();
+            for s in &p.sig {
+                match self.get_tuple(s) {
+                    Some(ts) => {
+                        for t in ts.iter() {
+                            new_sigs.push(t.clone());
+                        }
+                    }
+                    None => {
+                        new_sigs.push(s.clone());
+                    }
+                }
+            }
+            let p = crate::info::Pred::new(p.name.clone(), p.idx, new_sigs);
+            new_preds.push(p);
+        }
+        // 1. iter all the clauses
+        for c in self.instance.clauses() {
+            let c = self.work_on(c);
+        }
+        self.instance.rm_args(to_keep)
+    }
 }
 
 #[test]
@@ -60,22 +140,9 @@ impl RedStrat for InlineADT {
     }
 
     fn apply(&mut self, instance: &mut PreInstance) -> Res<RedInfo> {
-        let mut typ2tup = HashMap::new(); // O(n) is OK
-        let mut n_types = 0; // count number of types for generating unique names later
-        for (name, typ) in dtyp::get_all().iter() {
-            n_types += 1;
-            match tuple_opt(typ) {
-                Some(types) => {
-                    println!("name: {}", name);
-                    println!("typ: {}", typ);
-                    println!("types: {:?}", types);
-                    typ2tup.insert(typ, types);
-                }
-                None => {}
-            }
-        }
-
         // expand the tuple
+        let mut trans = Trans::new(instance);
+        trans.expand_tuple();
         unimplemented!()
     }
 }
